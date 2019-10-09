ea80fcdb3cf4ed34bb6b4a367335592c25b55fe4
Edson Manuel Panduro Chota <edson.panduro@tecsup.edu.pe>
2019-10-09 10:18:42 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Edson_Manuel on 10/9/19.
+//  Copyright © 2019 Edson_Manuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
